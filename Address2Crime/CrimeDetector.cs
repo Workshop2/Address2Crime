50273a7c040ce942a23c4ec2e31d75720be84bb6
Simon Colmer <simon_colmer@hotmail.com>
2015-07-15 21:04:51 +0100

    Pulling out crimes

---
+++ Address2Crime/CrimeDetector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PoliceUk;
+using PoliceUk.Entities.StreetLevel;
+
+namespace Address2Crime
+{
+    public class CrimeDetector
+    {
+        public StreetLevelCrimeResults GetCrimesForLocationThisYear(Geoposition geoposition)
+        {
+            var policeClient = new PoliceUkClient();
+            return policeClient.StreetLevelCrimes(geoposition, new DateTime(2015, 05, 01));
+        }
+    }
+}
---
+++ Address2Crime/CrimeDetectorTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using PoliceUk;
+using PoliceUk.Entities.StreetLevel;
+using Should;
+using SpecsFor;
+
+namespace Address2Crime
+{
+    public class CrimeDetectorTests : SpecsFor<CrimeDetector>
+    {
+        public StreetLevelCrimeResults Result { get; set; }
+        private readonly Geoposition _geoposition = new Geoposition(51.4902435, -0.3005307);
+
+        protected override void When()
+        {
+            Result = SUT.GetCrimesForLocationThisYear(_geoposition);
+        }
+
+        [Test]
+        public void then_should_return_data()
+        {
+            Result.ShouldNotBeNull();   
+        }
+    }
+}
