96facb739742442ce6497e3e1fb372b91daa03f0
Simon Colmer <simon_colmer@hotmail.com>
2015-07-15 20:49:51 +0100

    Adding basic tests

---
+++ Address2Crime/Coordinator.cs
@@ -0,0 +1,12 @@
+﻿using PoliceUk;
+
+namespace Address2Crime
+{
+    public class Coordinator
+    {
+        public Geoposition GetCoordinatesFromAddress(string address)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Address2Crime/Example.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using PoliceUk;
+using Should;
+using SpecsFor;
+
+namespace Address2Crime
+{
+    public class Example : SpecsFor<Coordinator>
+    {
+        public Geoposition Result { get; set; }
+        private readonly string Address = "302 Laval House, Ealing Road, Brentford, TW8 0GL";
+        
+        protected override void When()
+        {
+            Result = SUT.GetCoordinatesFromAddress(Address);
+        }
+
+        [Test]
+        public void then_should_return_data()
+        {
+            Result.ShouldNotBeNull();
+            Result.Latitude.ShouldNotEqual(0);
+            Result.Latitude.ShouldNotEqual(0);
+        }
+    }
+}
